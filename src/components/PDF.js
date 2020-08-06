d81f2f30913d2db5886e6876f9f94582e18771b7
Dhruv Kumar Jha <dhruv@dhruvkumarjha.com>
2020-08-06 19:16:53 +0530

    Fixed max callstack for base64 conversion

--- src/components/PDF.js
+++ src/components/PDF.js
@@ -2,8 +2,21 @@ import React from 'react';
 import styled from 'styled-components';
 
 
+const abToBase64 = ( buffer ) => {
+  let binary = '';
+  let bytes = new Uint8Array( buffer );
+  let len = bytes.byteLength;
+  for (let i = 0; i < len; i++) {
+      binary += String.fromCharCode( bytes[ i ] );
+  }
+  return window.btoa( binary );
+}
+
+
 const PDFComponent = (props) => {
-  let base64String = btoa(String.fromCharCode(...props.data));
+  // let base64String = btoa(String.fromCharCode(...props.data));
+  const base64String = abToBase64(props.data);
+
   return (
     <Container>
       <PDF src={`data:application/pdf;base64,${base64String}`} />
